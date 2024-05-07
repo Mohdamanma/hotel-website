db1a5f12258b4cad5457a292c357b381eafbb5e0
muhammed aman ma <112202255+Mohdamanma@users.noreply.github.com>
2024-05-07 18:20:30 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var element = document.getElementById("navbar")
+    element.classList.toggle("flex-col")
+    var elements = document.querySelector("#menus")
+    elements.classList.toggle("ulhamb")
+    console.log("running")
+ }
